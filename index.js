1676a0f61f12e35633a7f5d75ca2ea68ceec082a
leoeliel <leoeliel13@gmail.com>
2024-04-19 13:23:43 -0400

    adicionando forma aprimorada de ver o resultado do calculo de fardos

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 let mainForm = document.getElementById("mainForm");
+let reset = document.getElementById("reset");
+let button = document.getElementById("send"); 
+
+let productMeasure = document.getElementById("productMeasure");
+let shelfDeepness = document.getElementById("shelfDeepness");
+let qtdRows = document.getElementById("qtdRows"); 
+let unitsPerPack = document.getElementById("unitsPerPack");
 
 mainForm.addEventListener("submit", (ev) => {
+
     ev.preventDefault();
     sendData();
 });
 
+reset.addEventListener("click", (ev) => {
+
+    ev.preventDefault();
+    resetValues();
+});
+
 async function sendData() {
-    
-    // const formData = new formData(mainForm);
-    
-    let productMeasure = document.getElementById("productMeasure");
-    let shelfDeepness = document.getElementById("shelfDeepness");
-    let qtdRows = document.getElementById("qtdRows"); 
-    let unitsPerPack = document.getElementById("unitsPerPack");
 
     let packs = Math.trunc(((shelfDeepness.valueAsNumber/productMeasure.valueAsNumber)*qtdRows.valueAsNumber)/unitsPerPack.valueAsNumber);
+    button.innerHTML = `${packs} Fardo(s)`
+    // alert(`${packs} Fardos`);
 
-    console.log(`${packs} Fardos`);
-    alert(`${packs} Fardos`);
+}
 
+async function resetValues() {
+
+    productMeasure.innerHTML = "";
+    shelfDeepness.innerHTML = "";
+    qtdRows.innerHTML = "";
+    unitsPerPack.innerHTML = "";
 }
